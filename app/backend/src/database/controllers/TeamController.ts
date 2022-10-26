a5f99be9100e272284850ba7602da5b4702aa88b
vtrtga <vitor.ypvalim@gmail.com>
2022-10-26 15:31:54 -0400

    Team Controller implementation

---
+++ app/backend/src/database/controllers/TeamController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import TeamService from '../services/TeamService';
+
+class TeamController {
+  private readonly teamService: TeamService;
+
+  constructor() {
+    this.teamService = new TeamService();
+  }
+
+  getAll = async (_req: Request, res: Response) => {
+    const teams = await this.teamService.getAll();
+
+    return res.status(200).json(teams);
+  };
+
+  getById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const nId = Number(id);
+    const team = await this.teamService.getById(nId);
+    return res.status(200).json(team);
+  };
+}
+
+export default TeamController;
