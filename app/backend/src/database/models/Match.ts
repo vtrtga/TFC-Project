de2e8d00adcdb380bd67499b4a4c53a911e64a06
vtrtga <vitor.ypvalim@gmail.com>
2022-10-26 15:48:33 -0400

    Teams model and match model relations

--- app/backend/src/database/models/Match.ts
+++ app/backend/src/database/models/Match.ts
@@ -1,6 +1,7 @@
 import { BOOLEAN, INTEGER } from 'sequelize';
 import { Model } from 'sequelize/types';
 import db from '.';
+import Teams from './Teams';
 
 class Match extends Model {
   id: number;
@@ -11,39 +12,43 @@ class Match extends Model {
   inProgress: boolean;
 }
 
-Match.init(
-  {
-    id: {
-      type: INTEGER,
-      allowNull: false,
-      primaryKey: true,
-    },
-    homeTeam: {
-      type: INTEGER,
-      allowNull: false,
-    },
-    homeTeamGoals: {
-      type: INTEGER,
-      allowNull: false,
-    },
-    awayTeam: {
-      type: INTEGER,
-      allowNull: false,
-    },
-    awayTeamGoals: {
-      type: INTEGER,
-      allowNull: false,
-    },
-    inProgress: {
-      type: BOOLEAN,
-      allowNull: false,
-    },
+Match.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
   },
-  {
-    sequelize: db,
-    modelName: 'matches',
-    underscored: true,
-    timestamps: false,
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
   },
-);
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  underscored: true,
+  timestamps: false,
+});
+
+Match.belongsTo(Teams, { foreignKey: 'homeTeam', as: 'teamHome' });
+Match.belongsTo(Teams, { foreignKey: 'awayTeam', as: 'teamAway' });
+
+Teams.hasMany(Match, { foreignKey: 'homeTeam', as: 'teamHome' });
+Teams.hasMany(Match, { foreignKey: 'awayTeam', as: 'teamAway' });
+
 export default Match;
